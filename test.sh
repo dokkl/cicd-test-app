b25e2bd1d86bafb0b501d4a1cb2c4dc481edaf24
남궁훈 <kant@sk.com>
2021-01-12 23:30:51 +0900

    [hoon-1]edit2

--- test.sh
+++ test.sh
@@ -2,12 +2,9 @@
 # jdk 11 설치
 sudo rpm --import https://yum.corretto.aws/corretto.key
 sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-sudo yum install -y java-11-corretto-devel
+sudo yum install -y java-11-amazon-corretto-devel
 
 # code deploy agent 설치
-sudo yum -y update
-sudo yum install -y ruby
-cd /home/ec2-user
-curl -o https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
-sudo chmod +x ./install
+aws s3 cp s3://aws-codedeploy-ap-northeast-2/latest/install . --region ap-northeast-2
+chmod +x ./install
 sudo ./install auto
